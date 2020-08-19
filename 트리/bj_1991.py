c7aaadf2fef6d895e8afaeb8e7a252429d2d49a7
baeyuna <baeyuna@MacBook-Pro.local>
2020-08-19 21:03:44 +0900

    정렬

--- 트리/bj_1991.py
+++ 트리/bj_1991.py
@@ -6,24 +6,33 @@ class Node:
         self.right_node = right_node
         
 def pre_order(node):
+    # 루트
     print(node.value, end='')
+    # 왼쪽 자식
     if node.left_node != '.':
         pre_order(tree[node.left_node])
+    # 오른쪽 자식
     if node.right_node != '.':
         pre_order(tree[node.right_node])
 
 def in_order(node):
+    # 왼쪽 자식
     if node.left_node != '.':
         in_order(tree[node.left_node])
+    # 루트 
     print(node.value, end='')
+    # 오른쪽 자식 
     if node.right_node != '.':
         in_order(tree[node.right_node])
 
 def post_order(node):
+    # 왼쪽 자식 
     if node.left_node != '.':
         post_order(tree[node.left_node])
+    # 오른쪽 자식 
     if node.right_node != '.':
         post_order(tree[node.right_node])
+    # 루트s
     print(node.value, end='')
 
 # 입력값 받기    
